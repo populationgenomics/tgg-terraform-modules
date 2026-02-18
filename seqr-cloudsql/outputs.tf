4047f5734fa6480a3e65e0d2670536bdf9bcee88
Milo Hyben <milo.hyben@gmail.com>
2026-02-18 14:09:59 +1100

    Update seqr-cloudsql with more options.

--- seqr-cloudsql/outputs.tf
+++ seqr-cloudsql/outputs.tf
@@ -1,3 +1,8 @@
 output "cloudsql-instance-name" {
   value = google_sql_database_instance.postgres_cloudsql_instance.name
 }
+
+output "cloudsql-connection-name" {
+  description = "The connection name of the master instance to be used in connection strings"
+  value = google_sql_database_instance.postgres_cloudsql_instance.connection_name
+}
