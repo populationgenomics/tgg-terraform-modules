02ce5d87cb2929c7ebe457a906cac0ea65a0bfc2
Milo Hyben <milo.hyben@gmail.com>
2026-02-18 14:22:44 +1100

    Customise seqr-gke.

--- seqr-gke/outputs.tf
+++ seqr-gke/outputs.tf
@@ -1,3 +1,13 @@
 output "gke-cluster-name" {
   value = google_container_cluster.cluster.name
 }
+
+# cluster API endpoint
+output "gke_cluster_api_endpoint" {
+  value = google_container_cluster.cluster.endpoint
+}
+
+# cluster CA certificate
+output "gke_cluster_ca_cert" {
+  value = google_container_cluster.cluster.master_auth[0].cluster_ca_certificate
+}
