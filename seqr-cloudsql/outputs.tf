0642374e95b95fd765840084408ec3afd5db15e5
Milo Hyben <milo.hyben@gmail.com>
2026-03-10 10:47:06 +1100

    Add cloudsql-private-ip output to seqr-cloudsql.

--- seqr-cloudsql/outputs.tf
+++ seqr-cloudsql/outputs.tf
@@ -6,3 +6,8 @@ output "cloudsql-connection-name" {
   description = "The connection name of the master instance to be used in connection strings"
   value = google_sql_database_instance.postgres_cloudsql_instance.connection_name
 }
+
+output "cloudsql-private-ip" {
+  value       = google_sql_database_instance.postgres_cloudsql_instance.private_ip_address
+  description = "The private IP address of the main SQL server instance."
+}
